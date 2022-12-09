3ce73f98c4972b7cde65979d2aa72d7a3f2f2b37
p_stolz <p_stolz@hood-group.com>
2022-12-09 15:13:25 +0100

    Runde_1_Steps_angepasst

--- System/test/system/bdd/steps/EinkaufslisteSteps.java
+++ System/test/system/bdd/steps/EinkaufslisteSteps.java
@@ -1,5 +1,10 @@
 package system.bdd.steps;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,8 +14,8 @@ import system.outsideinteraction.SchnittstelleZumKonsument;
 
 public class EinkaufslisteSteps implements SchnittstelleZumKonsument {
 
-	@SuppressWarnings("unused")
 	private SchnittstelleVomKonsument app;	
+	private Collection<String> einkaufsliste = null;
 	
 	public EinkaufslisteSteps() {
 		app = new EinkaufsApp(this);
@@ -18,21 +23,25 @@ public class EinkaufslisteSteps implements SchnittstelleZumKonsument {
 	
 	@Given("eine leere Einkaufsliste")
 	public void eine_leere_einkaufsliste() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		// intentionally left empty, nothing has to be done to get a leere Einkaufsliste
 	}
 	
 	@When("ich schreibe {string} in die Einkaufsliste")
-	public void ich_schreibe_in_die_einkaufsliste(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void ich_schreibe_in_die_einkaufsliste(String eintrag) {
+		app.schreibeInEinkaufsliste(eintrag);
 	}
 	
 	@Then("die Einkaufsliste sollte den Eintrag {string} enthalten.")
-	public void die_einkaufsliste_sollte_den_eintrag_enthalten(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}	
+	public void die_einkaufsliste_sollte_den_eintrag_enthalten(String eintrag) {
+		assertNotNull(einkaufsliste);
+		assertTrue(einkaufsliste.contains(eintrag));
+	}
+
+	@Override
+	public void zeigeEinkaufsliste(Collection<String> einkaufsliste) {
+		this.einkaufsliste = einkaufsliste;
+	}
+	
 }
 
 
