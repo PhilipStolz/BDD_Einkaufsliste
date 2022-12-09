b313564316f636e5341f18d14fd3ad393bdc2bdc
p_stolz <p_stolz@hood-group.com>
2022-12-09 14:49:42 +0100

    Runde_1_Steps_Rümpfe

--- System/test/system/bdd/steps/EinkaufslisteSteps.java
+++ System/test/system/bdd/steps/EinkaufslisteSteps.java
@@ -1,5 +1,8 @@
 package system.bdd.steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import system.EinkaufsApp;
 import system.outsideinteraction.SchnittstelleVomKonsument;
 import system.outsideinteraction.SchnittstelleZumKonsument;
@@ -12,6 +15,24 @@ public class EinkaufslisteSteps implements SchnittstelleZumKonsument {
 	public EinkaufslisteSteps() {
 		app = new EinkaufsApp(this);
 	}
+	
+	@Given("eine leere Einkaufsliste")
+	public void eine_leere_einkaufsliste() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("ich schreibe {string} in die Einkaufsliste")
+	public void ich_schreibe_in_die_einkaufsliste(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("die Einkaufsliste sollte den Eintrag {string} enthalten.")
+	public void die_einkaufsliste_sollte_den_eintrag_enthalten(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}	
 }
 
 
